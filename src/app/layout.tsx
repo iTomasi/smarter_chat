5458caf2404a9dc4e59198d62af5436304f80a72
Tomas Duclos <tomasmarcelo.duclos@gmail.com>
2023-04-20 20:51:42 -0400

    favicon and text-sm global

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from 'next'
 import type { ReactNode } from 'react'
 import ToasterSonner from 'components/ToasterSonner'
 import { Inter } from 'next/font/google'
@@ -17,13 +18,7 @@ export default function Layout ({
 }: Props) {
   return (
     <html className="dark">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Clone Chat gpt</title>
-      </head>
-      <body className="dark:bg-stone-950 dark:text-white" style={inter.style}>
+      <body className="dark:bg-stone-950 dark:text-white text-sm" style={inter.style}>
         <ToasterSonner />
         <main>
           <ApiKeyProvider>
@@ -33,4 +28,11 @@ export default function Layout ({
       </body>
     </html>
   )
+}
+
+export const metadata: Metadata = {
+  title: 'Clone Chat gpt',
+  icons: {
+    icon: '/favicon.ico'
+  }
 }
