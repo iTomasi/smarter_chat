97b16ec17650f2987e9cbab64b183558939210a5
Tomas Duclos <duclosiw@gmail.com>
2023-04-21 02:16:04 -0400

    meta viewport for zoom in zoom out

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,5 +35,5 @@ export const metadata: Metadata = {
   icons: {
     icon: '/favicon.ico'
   },
-  viewport: 'width=device-width, initial-scale=1.0'
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0'
 }
