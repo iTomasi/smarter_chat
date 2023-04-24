099bb10e81cbc0ae19e5ebddeb71f1effa643960
Tomas Duclos <duclosiw@gmail.com>
2023-04-24 01:07:10 -0400

    sidebar max-h ChatLink

--- src/components/home/SideBar/Top/index.tsx
+++ src/components/home/SideBar/Top/index.tsx
@@ -1,15 +1,20 @@
 import NewChatButton from './NewChatButton'
 import ChatLinkCard from './ChatLinkCard'
 
+
 export default function Top () {
 
   return (
     <div>
-      <div>
-        <ChatLinkCard
+      <div className="max-h-[calc(100vh-14rem)] lg:max-h-[calc(100vh-9rem)] overflow-y-auto">
+        {
+          Array.from(Array(5).keys()).map(() => (
+            <ChatLinkCard
           id={2}
           name="Test"
         />
+          ))
+        }
       </div>
       <NewChatButton/>
     </div>
