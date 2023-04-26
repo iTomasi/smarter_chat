14bc1bb19ca903f8efd7c08942d26f7ab36c9612
Tomas Duclos <duclosiw@gmail.com>
2023-04-26 13:48:21 -0400

    edit message component (ia left && me right)

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -8,7 +8,7 @@ export default function Message ({
   content
 }: Props) {
   return (
-    <div className={`flex gap-2 ${from === 'gpt' ? 'flex-row-reverse' : 'flex-row'}`}>
+    <div className={`flex gap-2 ${from === 'gpt' ? 'flex-row' : 'flex-row-reverse'}`}>
       <div className={`min-w-[3rem] max-w-[3rem] min-h-[3rem] max-h-[3rem] rounded-full ${from === 'gpt' ? 'bg-fuchsia-500' : 'bg-sky-500'}`}></div>
 
       <div className="bg-gray-200 dark:bg-stone-800 p-3 rounded-md break-words max-w-[75ch]">
